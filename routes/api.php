7f335e77966a90c93c5e19bf4d49eb6497505d32
Mohammad Khafid Barokum <navigator619@gmail.com>
2020-07-29 01:07:37 +0700

    product endpoint init

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'product'], function () {
+    Route::get('/', 'ProductController@index');
+    Route::post('/', 'ProductController@store');
+    Route::get('/{id}', 'ProductController@show');
+    Route::delete('/{id}', 'ProductController@destroy');
+});
